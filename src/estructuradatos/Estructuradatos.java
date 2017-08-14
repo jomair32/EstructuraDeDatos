9b3b6df06b4d056e5ccb446ca11b5825b932da6d
DESKTOP-OAF8639\jomai <a01373791@itesm.mx>
2017-08-14 10:23:12 -0500

    inicial 14/8/17

---
+++ src/estructuradatos/Estructuradatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructuradatos;
+
+/**
+ *
+ * @author jomai
+ */
+public class Estructuradatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+        // TODO code application logic here
+    }
+    
+}
